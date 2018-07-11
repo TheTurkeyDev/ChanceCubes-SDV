e4e13e9b31822a360d63af46a5b2657e84657de6
Turkey <turkey2349@gmail.com>
2018-07-10 23:58:59 -0400

    Initial commit. CHANCE CUBES IN STARDEW VALLEY

---
+++ Chance Cubes/CCubesCore.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+
+namespace Chance_Cubes
+{
+    public class CCubesCore : Mod
+    {
+        private CCubesConfig config;
+        private Texture2D giantRingTexture;
+        public override void Entry(IModHelper helper)
+        {
+            this.Monitor.Log("Chance Cubes has loaded!");
+            this.giantRingTexture = this.Helper.Content.Load<Texture2D>("assets/chance_cube.png");
+            ChanceCube.texture = this.giantRingTexture;
+        }
+    }
+}
---
+++ Chance Cubes/ChanceCube.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using System;
+
+namespace Chance_Cubes
+{
+    class ChanceCube : StardewValley.Object
+    {
+        public static Texture2D texture;
+
+        public ChanceCube()
+        {
+            this.name = "Chance Cube";
+            this.type = "interactive";
+
+            this.boundingBox = new Rectangle((int)this.tileLocation.X * Game1.tileSize, (int)this.tileLocation.Y * Game1.tileSize, Game1.tileSize, Game1.tileSize);
+        }
+
+        public ChanceCube(Vector2 tileLocation)
+        {
+            this.name = "Chance Cube";
+            this.type = "interactive";
+            this.tileLocation = tileLocation;
+            this.boundingBox = new Rectangle((int)this.tileLocation.X * Game1.tileSize, (int)this.tileLocation.Y * Game1.tileSize, Game1.tileSize, Game1.tileSize);
+        }
+
+        public override string getDescription()
+        {
+            return "What's in the cube?";
+        }
+
+        public override void drawInMenu(SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, bool drawStackNumber)
+        {
+            spriteBatch.Draw(texture, location + new Vector2(Game1.tileSize / 2, Game1.tileSize / 2) * scaleSize,
+                Game1.getSourceRectForStandardTileSheet(texture, 0, 64, 64), Color.White * transparency, 0.0f,
+                new Vector2(32f, 32f) * scaleSize, (scaleSize * Game1.pixelZoom) / 4f, SpriteEffects.None, layerDepth);
+        }
+
+        public override void drawWhenHeld(SpriteBatch spriteBatch, Vector2 objectPosition, StardewValley.Farmer f)
+        {
+            spriteBatch.Draw(texture, objectPosition, 
+                new Rectangle?(Game1.getSourceRectForStandardTileSheet(texture, 0, 64, 64)), 
+                Color.White, 0.0f, Vector2.Zero, Game1.pixelZoom / 4f, SpriteEffects.None, 
+                Math.Max(0.0f, (float)(f.getStandingY() + 2) / 10000f));
+        }
+    }
+}
