596e06d36c766444110921cae00ccf7ad01a4403
Turkey <turkey2349@gmail.com>
2018-07-11 14:57:37 -0400

    Added some comments

--- Chance Cubes/CCubesCore.cs
+++ Chance Cubes/CCubesCore.cs
@@ -7,13 +7,16 @@ namespace Chance_Cubes
     public class CCubesCore : Mod
     {
         private CCubesConfig config;
-        private Texture2D giantRingTexture;
+        private Texture2D chanceCubeTexture;
         public override void Entry(IModHelper helper)
         {
             RewardRegistry.initRewards();
 
-            this.giantRingTexture = this.Helper.Content.Load<Texture2D>("assets/chance_cube.png");
-            ChanceCube.texture = this.giantRingTexture;
+            /*  Gets the texture that we will use for the Chance Cube. It feels weird to do it here, 
+                but this is how the example i was following did it.
+            */
+            this.chanceCubeTexture = this.Helper.Content.Load<Texture2D>("assets/chance_cube.png");
+            ChanceCube.texture = this.chanceCubeTexture;
 
             this.Monitor.Log("Chance Cubes has loaded!");
         }
--- Chance Cubes/ChanceCube.cs
+++ Chance Cubes/ChanceCube.cs
@@ -7,8 +7,12 @@ namespace Chance_Cubes
 {
     class ChanceCube : StardewValley.Object
     {
+        //The texture of the Chance Cube
         public static Texture2D texture;
 
+        /*  I'm not totally positive which constructors are needed and which aren't 
+            since I don't have the item placed in the world yet. Only in the players inventory
+        */
         public ChanceCube()
         {
             this.name = "Chance Cube";
@@ -25,24 +29,37 @@ namespace Chance_Cubes
             this.boundingBox = new Rectangle((int)this.tileLocation.X * Game1.tileSize, (int)this.tileLocation.Y * Game1.tileSize, Game1.tileSize, Game1.tileSize);
         }
 
+        // Tool tip info
         public override string getDescription()
         {
             return "What's in the cube?";
         }
 
+        //Draws the Item in the menu
         public override void drawInMenu(SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, bool drawStackNumber)
         {
-            spriteBatch.Draw(texture, location + new Vector2(Game1.tileSize / 2, Game1.tileSize / 2) * scaleSize,
-                Game1.getSourceRectForStandardTileSheet(texture, 0, 64, 64), Color.White * transparency, 0.0f,
-                new Vector2(32f, 32f) * scaleSize, (scaleSize * Game1.pixelZoom) / 4f, SpriteEffects.None, layerDepth);
+            spriteBatch.Draw(texture, // The texture to use. Most items use the game larger texture sheet
+                location + new Vector2(Game1.tileSize / 2, Game1.tileSize / 2) * scaleSize, // Location on the screen to render at
+                Game1.getSourceRectForStandardTileSheet(texture, 0, 64, 64), // The area of the texture to render (so I guess usefull for texture sheets with many textures)
+                Color.White * transparency, // White color seems to render the texture normally. This may be used for tinting textures?
+                0.0f, // Rotation
+                new Vector2(32f, 32f) * scaleSize, // Not quite sure what this is, but it affects where the texture is drawn on the screen
+                (scaleSize * Game1.pixelZoom) / 4f, //scaleSize * Game1.pixelZoom is the default. I divide by 4 to fit my 64 by 64 texture
+                SpriteEffects.None, // No effects needed 
+                layerDepth); // IDK
         }
 
         public override void drawWhenHeld(SpriteBatch spriteBatch, Vector2 objectPosition, StardewValley.Farmer f)
         {
-            spriteBatch.Draw(texture, objectPosition, 
-                new Rectangle?(Game1.getSourceRectForStandardTileSheet(texture, 0, 64, 64)), 
-                Color.White, 0.0f, Vector2.Zero, Game1.pixelZoom / 4f, SpriteEffects.None, 
-                Math.Max(0.0f, (float)(f.getStandingY() + 2) / 10000f));
+            spriteBatch.Draw(texture, // Texture / Sheet of textures to use
+                objectPosition, // Position to draw the texture at. Not sure if screen or world relative
+                new Rectangle?(Game1.getSourceRectForStandardTileSheet(texture, 0, 64, 64)), // Same as above
+                Color.White, // Same as above
+                0.0f, // Still same
+                Vector2.Zero, // Not sure why this one is zero and the one above isn't
+                Game1.pixelZoom / 4f, // I divide by 4 for the same reason as above
+                SpriteEffects.None, // No effects needed
+                Math.Max(0.0f, (float)(f.getStandingY() + 2) / 10000f)); // IDK again
         }
     }
 }
