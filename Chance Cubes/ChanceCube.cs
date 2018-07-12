9a7ede1a7bec865a8127fe34022454c343f210f0
Turkey <turkey2349@gmail.com>
2018-07-12 16:26:21 -0400

    Working on adding support to trigger rewards

--- Chance Cubes/ChanceCube.cs
+++ Chance Cubes/ChanceCube.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Chance_Cubes.Reward;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
+using StardewValley.Locations;
 using System;
+using System.Collections.Generic;
 
 namespace Chance_Cubes
 {
@@ -35,6 +38,18 @@ namespace Chance_Cubes
             return "What's in the cube?";
         }
 
+        // Whether or not the object can be placed in the world
+        public override bool isPlaceable()
+        {
+            return true;
+        }
+
+        //
+        public override void performRemoveAction(Vector2 tileLocation, GameLocation environment)
+        {
+            RewardRegistry.triggerRandomReward(tileLocation, Game1.player);
+        }
+
         //Draws the Item in the menu
         public override void drawInMenu(SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, bool drawStackNumber)
         {
@@ -48,7 +63,8 @@ namespace Chance_Cubes
                 SpriteEffects.None, // No effects needed 
                 layerDepth); // IDK
         }
-
+        
+        // Draws the item above the players head when they hold it
         public override void drawWhenHeld(SpriteBatch spriteBatch, Vector2 objectPosition, StardewValley.Farmer f)
         {
             spriteBatch.Draw(texture, // Texture / Sheet of textures to use
@@ -60,6 +76,20 @@ namespace Chance_Cubes
                 Game1.pixelZoom / 4f, // I divide by 4 for the same reason as above
                 SpriteEffects.None, // No effects needed
                 Math.Max(0.0f, (float)(f.getStandingY() + 2) / 10000f)); // IDK again
+        }
+
+        // I Think this is called when the object is being drawn in the world
+        public override void draw(SpriteBatch spriteBatch, int x, int y, float alpha = 1f)
+        {
+            spriteBatch.Draw(texture, // Texture / Sheet of textures to use
+                Game1.GlobalToLocal(Game1.viewport, new Vector2((float)(x * Game1.tileSize), (float)(y * Game1.tileSize - (Game1.pixelZoom - this.boundingBox.Height)))), // Position to draw the texture on the screen. The location needs to be converted from World coordinates to screen cordinates
+                new Rectangle?(Game1.getSourceRectForStandardTileSheet(texture, 0, 64, 64)), // Same as above
+                Color.White * alpha, // Same as above
+                0.0f, // Still same
+                Vector2.Zero, // Not sure why this one is zero and the menu one above isn't
+                (float)Game1.pixelZoom,
+                SpriteEffects.None, // No effects needed
+                (float)(this.boundingBox.Bottom - 8) / 10000f); // Still don't know
         }
     }
 }
--- Chance Cubes/Reward/RewardRegistry.cs
+++ Chance Cubes/Reward/RewardRegistry.cs
@@ -2,16 +2,24 @@
 
 using Chance_Cubes.Reward.Types;
 using Chance_Cubes.Reward.Parts;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace Chance_Cubes.Reward
 {
     class RewardRegistry
     {
+        private static Random rand = new Random();
         private static List<IReward> rewards = new List<IReward>();
 
         public static void initRewards()
         {
             rewards.Add(new BasicReward("Test 1", 0, new IType[]{ new ItemType(new ItemPart[]{ new ItemPart(1, 1)})}));
+        }
+
+        public static void triggerRandomReward(Vector2 pos, StardewValley.Farmer farmer)
+        {
+            rewards[rand.Next(rewards.Count)].Trigger(pos, farmer);
         }
     }
 }
